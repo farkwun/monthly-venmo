89272924767ed6de1872505fb3c96165f5069f5e
Bryan Chu <thisisbryanchu@gmail.com>
2024-05-03 02:30:12 -0700

    Modifies to take in Google creds

--- init.py
+++ init.py
@@ -3,61 +3,64 @@ from dotenv import load_dotenv
 from notifiers import get_notifier
 from datetime import datetime
 
-from utils import get_env, env_vars, get_month, Venmo, Telegram
+from utils import get_env, env_vars, get_month, Venmo, Telegram, GoogleDrive
+
 
 def main(now):
-  """
-  The main function which initiates the script.
-  """
+    """
+    The main function which initiates the script.
+    """
 
-  load_dotenv()  # take environment variables from .env.
-  actualVars = []
-  for var in env_vars:
-    actualVars.append(get_env(var))
+    load_dotenv()  # take environment variables from .env.
+    actualVars = []
+    for var in env_vars:
+        actualVars.append(get_env(var))
 
-  access_token, chat_id, bot_token, k_friend_id, c_friend_id, w_friend_id, j_friend_id = actualVars
+    (access_token, service_account_credentials, spreadsheet_key) = actualVars
 
-  month = get_month(now)
-  venmo = Venmo(access_token)
-  telegram = Telegram(bot_token, chat_id)
+    month = get_month(now)
+    venmo = Venmo(access_token)
+    google = GoogleDrive(
+        GoogleDrive.decode_service_credentials(service_account_credentials)
+    )
 
-  friends =[
-    {
-      "name": "KRam",
-      "id": k_friend_id,
-    },
-    {
-      "name": "Chrissy",
-      "id": c_friend_id,
-    },
-    {
-      "name": "Will",
-      "id": w_friend_id,
-    },
-  ]
+    friends = google.get_all_records_from_spreadsheet(spreadsheet_key)
 
-  successfulRequests = []
-  expectedRequests = len(friends)
+    successfulRequests = []
+    expectedRequests = len(friends)
 
-  for friend in friends:
-    name = friend["name"]
-    id = friend["id"]
-    description = "Spotify for the month of " + month + "— Sent by Joe's Assistant Efron 🤵🏻‍♂️"
-    amount = 3.50
-    message = f"""Good news old sport!
+    for friend in friends:
+        if friend.status != "ACTIVE" or friend.id is None:
+            continue
+        name = friend.username
+        id = friend.id
+        description = "Tribe tuition for the month of " + month + "— Sent by 👹"
+        amount = friend.tuition
+        message = f"""Good news old sport!
 
 I have successfully requested money from {name}.
 
 — Efron 🤵🏻‍♂️
     """
-    success = venmo.request_money(id, amount, description, telegram.send_message(message))
-    if success:
-      successfulRequests.append(success)
+        success = venmo.request_money(id, amount, description, lambda x: message)
+        if success:
+            successfulRequests.append(success)
 
-  if len(successfulRequests) == expectedRequests:
-    print("✅ Ran script successfully and sent " + str(expectedRequests) + " Venmo requests.")
-  else:
-    print("❌ Something went wrong. Only sent " + str(len(successfulRequests)) + "/" + str(expectedRequests) + " venmo requests.")
+    if len(successfulRequests) == expectedRequests:
+        print(
+            "✅ Ran script successfully and sent "
+            + str(expectedRequests)
+            + " Venmo requests."
+        )
+    else:
+        print(
+            "❌ Something went wrong. Only sent "
+            + str(len(successfulRequests))
+            + "/"
+            + str(expectedRequests)
+            + " venmo requests."
+        )
+
 
 now = datetime.now()
 main(now)
--- utils.py
+++ utils.py
@@ -1,44 +1,60 @@
 import os
 from venmo_api import Client, PaymentPrivacy
 from notifiers import get_notifier
+import gspread
+import json
+import base64
+from dataclasses import dataclass
+
+
+@dataclass
+class RequestUser:
+    username: str
+    id: str
+    tuition: float
+    status: str
+
 
 def get_env(env):
-  """
-  Verfies that an environment variable exists
-  and returns it.
+    """
+    Verfies that an environment variable exists
+    and returns it.
 
-  Exits script if not found.
-  """
-  if os.getenv(env):
-      print(f"✅ {env} is available in the environment.")
-      return os.getenv(env)
-  else:
-      print(f"❌ Can't find {env} in environment.")
-      print("   Exiting script. Please add and run again.")
-      quit()
+    Exits script if not found.
+    """
+    if os.getenv(env):
+        print(f"✅ {env} is available in the environment.")
+        return os.getenv(env)
+    else:
+        print(f"❌ Can't find {env} in environment.")
+        print("   Exiting script. Please add and run again.")
+        quit()
 
-env_vars = ["VENMO_ACCESS_TOKEN", "TELEGRAM_CHAT_ID", "TELEGRAM_BOT_TOKEN", "K_FRIEND_ID", "C_FRIEND_ID", "W_FRIEND_ID", "J_FRIEND_ID"]
+
+env_vars = ["VENMO_ACCESS_TOKEN", "SERVICE_ACCOUNT_CREDENTIALS", "SPREADSHEET_KEY"]
+
 
 def verify_env_vars(vars, numOfExpected):
-  """
-  Verifies the list of vars are defined in the environment.
-  """
+    """
+    Verifies the list of vars are defined in the environment.
+    """
 
-  availableEnvVars = []
+    availableEnvVars = []
 
-  for var in vars:
-    # If it returns the env, which would be True
-    # then we know it's available
-    if get_env(var):
-        availableEnvVars.append(var)
+    for var in vars:
+        # If it returns the env, which would be True
+        # then we know it's available
+        if get_env(var):
+            availableEnvVars.append(var)
 
-  if len(availableEnvVars) == numOfExpected:
-    return True
-  else:
-    # This will technically never run
-    # because if one doesn't exist, then get_env quits
-    # but adding here for posterity
-    return False
+    if len(availableEnvVars) == numOfExpected:
+        return True
+    else:
+        # This will technically never run
+        # because if one doesn't exist, then get_env quits
+        # but adding here for posterity
+        return False
+
 
 def get_env_vars(vars):
     """
@@ -51,6 +67,7 @@ def get_env_vars(vars):
 
     return allVars
 
+
 def get_month(now):
     """
     Returns the current month.
@@ -60,27 +77,57 @@ def get_month(now):
     month = now.strftime("%B")
     return month
 
+
 class Venmo:
     def __init__(self, access_token):
         self.client = Client(access_token=access_token)
 
     def get_user_id_by_username(self, username):
         user = self.client.user.get_user_by_username(username=username)
-        if (user):
+        if user:
             return user.id
         else:
             print("ERROR: user did not comeback. Check username.")
             return None
 
-    def request_money(self, id, amount, description, callback = None):
+    def request_money(self, id, amount, description, callback=None):
         # Returns a boolean: true if successfully requested
-        return self.client.payment.request_money(amount, description, id, PaymentPrivacy.PUBLIC, None, callback)
+        return self.client.payment.request_money(
+            amount, description, id, PaymentPrivacy.PRIVATE, None, callback
+        )
+
 
 class Telegram:
     def __init__(self, bot_token, chat_id):
         self.bot_token = bot_token
         self.chat_id = chat_id
-        self.client = get_notifier('telegram')
+        self.client = get_notifier("telegram")
 
     def send_message(self, message):
         self.client.notify(message=message, token=self.bot_token, chat_id=self.chat_id)
+
+
+class GoogleDrive:
+    def __init__(self, service_credentials):
+        self.client = gspread.service_account_from_dict(service_credentials)
+
+    def get_all_records_from_spreadsheet(self, spreadsheet_key, worksheet_index=0):
+        VENMO_ACCESS_TOKEN = get_env("VENMO_ACCESS_TOKEN")
+        venmo = Venmo(VENMO_ACCESS_TOKEN)
+
+        # see sample.csv for an example of how this should look
+        sheet = self.client.open_by_key(spreadsheet_key)
+        worksheet = sheet.get_worksheet(worksheet_index)
+        records = worksheet.get_all_records()
+
+        return [
+            RequestUser(
+                **record, **{"id": venmo.get_user_id_by_username(record["username"])}
+            )
+            for record in records
+        ]
+
+    @classmethod
+    def decode_service_credentials(cls, base64_encoded_credentials):
+        encoded_key = str(base64_encoded_credentials)[2:-1]
+        return json.loads(base64.b64decode(encoded_key).decode("utf-8"))
